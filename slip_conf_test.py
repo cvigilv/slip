2259927f6c1a787848878f7d40a23c75372106d7
Carlos Vigil Vásquez <cvigil2@uc.cl>
2019-04-05 20:31:02 -0300

    Added new options and wrote default values for configs

--- slip_conf_test.py
+++ slip_conf_test.py
@@ -1,19 +1,20 @@
 [Input]
-Interactions file =
+Interactions file = /Users/cvigilv/Dropbox/Chembl22_goldStd3_max.txt.co.pokfilt.fp2.k1.LV.out.bugfixed.sorted.Fold
 Broad file =
 ChEMBL version =
 
 [Options]
 Keep target =
 Keep ligand =
-Pfam cutoff =
-Top X entries =
-min(Similarity) =
-max(Similarity) =
-min(Clinical Phase) =
-max(Clinical Phase) =
+Pfam cutoff = 1
+Top X entries = 100
+Similarity measure used = Srel
+min(Similarity) = 0
+max(Similarity) = 1
+min(Clinical Phase) = 0
+max(Clinical Phase) = 4
 
 [Output]
 Output directory =
 Output file =
-Generate plots =
+Generate plots = True
