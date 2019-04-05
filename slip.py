4d5e6397c0246a4b79294882eb0b565ec740dc03
Carlos Vigil Vásquez <cvigil2@uc.cl>
2019-04-05 20:30:03 -0300

    Added output plot for distribution of similarity measure, splitted by TP value

--- slip.py
+++ slip.py
@@ -8,10 +8,11 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from configparser import ConfigParser
 
 # SETTINGS
-config = ConfigParser()
-config.read(sys.argv[2])
+configs = ConfigParser()
+configs.read(sys.argv[1])
 
 Input_Interactions = configs.get('Input', 'Interactions file')
 Input_Broad        = configs.get('Input', 'Broad file')
@@ -20,6 +21,7 @@ Input_ChEMBL       = configs.get('Input', 'ChEMBL version')
 Opt_JustTarget       = configs.get('Options', 'Keep target')
 Opt_JustLigand       = configs.get('Options', 'Keep ligand')
 Opt_PfamCutoff       = configs.getint('Options', 'Pfam cutoff')
+Opt_SimilarityMeasure= configs.get('Options', 'Similarity measure used')
 Opt_TopX             = configs.getint('Options', 'Top X entries')
 Opt_minSimilarity    = configs.getint('Options', 'min(Similarity)')
 Opt_maxSimilarity    = configs.getint('Options', 'max(Similarity)')
@@ -38,5 +40,16 @@ print('SchuellerLab Ligand Priorization Pipepline - version {v}'.format(v = __ve
 in_df = pd.read_csv(Input_Interactions,
                     sep = '\t',
                     names = ['Fold', 'Query ligand ChEMBL ID', 'Hit target ChEMBL ID', 'Similarity measure', 'Hit ligand ID', 'Query target ID', 'TP'],
-                    header = False,
+                    header = None,
                     index_col = False) # Hardcoded options
+
+print(in_df.head())
+print(in_df.describe())
+
+sns.boxplot(x = 'TP', y = 'Similarity measure', data = in_df)
+plt.ylabel(Opt_SimilarityMeasure)
+plt.show()
+
+sns.violinplot(x = 'TP', y = 'Similarity measure', data = in_df, cut = 0, split = True)
+plt.ylabel(Opt_SimilarityMeasure)
+plt.show()
