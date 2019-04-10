f8f1a99c578a640d1c1a46c2c588f5ac298962f7
Carlos Vigil Vásquez <cvigil2@uc.cl>
2019-04-10 15:19:50 -0400

    Relative frequency histograms for distributions

--- slip.py
+++ slip.py
@@ -55,15 +55,26 @@ if Output_Plots == True:
     sns.boxplot(x = 'TP', y = 'Similarity measure', data = in_df)
     plt.ylabel(Opt_SimilarityMeasure)
     plt.title('{sim} distribution separated by TP'.format(sim = Opt_SimilarityMeasure))
-    plt.savefig(Input_Interactions+'.distribution.png', dpi = 300)
+    plt.savefig(Input_Interactions+'.distribution_boxplot.png', dpi = 300)
     plt.cla()
+
+    plt.hist(in_df[in_df['TP'] == 0]['Similarity measure'], density = True, color = 'blue', label = '0', alpha = 0.75, bins = [0.05*i for i in range(0,21)])
+    plt.hist(in_df[in_df['TP'] == 1]['Similarity measure'], density = True, color = 'orange', label = '1', alpha = 0.75, bins = [0.05*i for i in range(0,21)])
+    plt.xlabel(Opt_SimilarityMeasure)
+    plt.ylabel('Relative count (%)')
+    plt.legend(title = 'TP')
+    plt.title('{sim} distribution separated by TP'.format(sim = Opt_SimilarityMeasure))
+    plt.savefig(Input_Interactions+'.distribution_hist.png', dpi = 300)
+    plt.cla()
+
     print('Saving distribution plot for output file... DONE!')
+
 
 print('\nFiltering output by {sim} in range ]{min}, {max}]...'.format(sim = Opt_SimilarityMeasure, min = str(Opt_minSimilarity), max = str(Opt_maxSimilarity)), end = '\r')
 in_df = in_df[(in_df['Similarity measure'] <= Opt_maxSimilarity) & (in_df['Similarity measure'] > Opt_minSimilarity)]
 print('Filtering output by {sim} in range ]{min}, {max}]... DONE!'.format(sim = Opt_SimilarityMeasure, min = str(Opt_minSimilarity), max = str(Opt_maxSimilarity)))
-
 print(in_df.info(memory_usage = 'deep'))
+
 if Output_Plots == True:
     print('Saving distribution plot for filtered output file...',end = '\r')
     sns.boxplot(x = 'TP', y = 'Similarity measure', data = in_df)
@@ -71,4 +82,15 @@ if Output_Plots == True:
     plt.title('{sim} distribution for range ]{min}, {max}] separated by TP'.format(sim = Opt_SimilarityMeasure,  min = str(Opt_minSimilarity), max = str(Opt_maxSimilarity)))
     plt.savefig(Input_Interactions+'.filtered_distribution_1.png', dpi = 300)
     plt.cla()
+
+    bin_dif = (Opt_maxSimilarity - Opt_minSimilarity)/20
+    plt.hist(in_df[in_df['TP'] == 0]['Similarity measure'], density = True, color = 'blue', label = '0', alpha = 0.75, bins = [Opt_minSimilarity + bin_dif * i for i in range(0,21)])
+    plt.hist(in_df[in_df['TP'] == 1]['Similarity measure'], density = True, color = 'orange', label = '1', alpha = 0.75, bins = [Opt_minSimilarity + bin_dif * i for i in range(0,21)])
+    plt.xlabel(Opt_SimilarityMeasure)
+    plt.ylabel('Relative count (%)')
+    plt.legend(title = 'TP')
+    plt.title('{sim} distribution for range ]{min}, {max}] separated by TP'.format(sim = Opt_SimilarityMeasure,  min = str(Opt_minSimilarity), max = str(Opt_maxSimilarity)))
+    plt.savefig(Input_Interactions+'.distribution_hist_1.png', dpi = 300)
+    plt.cla()
+
     print('Saving distribution plot for filtered output file... DONE!')
