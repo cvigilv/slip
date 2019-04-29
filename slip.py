4c242551e015579e8d901a3a38e7cf4e81926b1b
Carlos Vigil Vásquez <cvigil2@uc.cl>
2019-04-29 17:08:12 -0400

    Added MySQL, Max Clinical Phase & T filters. Added common Pfam step also.

--- slip.py
+++ slip.py
@@ -5,7 +5,8 @@ __version__ = 0.0
 
 # DEPENDENCIES
 import sys
-import pandas as pd
+import os
+import pandas as pd 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -57,35 +58,6 @@ def TP_Histogram(df, column, states, labels, colours, filename, bins = [0.05*i f
     plt.savefig(filename, dpi = 300)
     plt.cla()
 
-def df_MySQL(df, chembl_version):
-    '''
-    Check if pair ligand-protein is annotated in ChEMBL database, useful for temporal validations and comparison with other species.
-    '''
-    db = pymysql.connect("localhost", "root", "123", Input_ChEMBL)      # Connect to MySQL server containing ChEMBL db
-
-    for index, row in df.iterrows():                                    # Iterate over each entry of the given dataframe
-        ligid = row['Query ligand ChEMBL ID']
-        trgid = row['Hit target ChEMBL ID']
-
-        cursor = db.cursor()                                            # Create cursor for MySQL 
-
-        sql_query = """SELECT * FROM activities act
-                JOIN molecule_dictionary    AS md ON act.molregno    = md.molregno
-                JOIN assays                 AS a  ON a.assay_id      = act.assay_id
-                JOIN target_dictionary      AS td ON a.tid           = td.tid
-                LEFT JOIN target_components AS tc ON td.tid          = tc.tid
-                LEFT JOIN component_domains AS cd ON tc.component_id = cd.component_id
-                LEFT JOIN domains           AS do ON cd.domain_id    = do.domain_id
-		WHERE md.chembl_id = '%s' AND td.chembl_id = '%s'""" % (ligid, trgid)
-
-        cursor.execute(sql_query)
-        
-        if cursor.rowcount <= 0:
-            df.loc[index, 'Found in {}'.format(Input_ChEMBL)] = False
-        else:
-            df.loc[index, 'Found in {}'.format(Input_ChEMBL)] = True
-       
-    db.close()
 
 # MAIN
 print('SchuellerLab Ligand Priorization Pipepline - version {v}'.format(v = __version__))
@@ -101,7 +73,7 @@ in_df = pd.read_csv(Input_Interactions,
 
 print('--> Cleaning output file from buggy entries...',end = '\r')
 in_df = in_df[in_df[Opt_SimilarityMeasure] >= 0]
-print('--> Cleaning output file from buggy entries... DONE!')
+print('--> Cleaning output file from buggy entries...'+'done!'.rjust(int(os.get_terminal_size().columns)))
 
 if Output_Plots == True:
     print('--> Saving distribution plot for output file...',end = '\r')
@@ -113,7 +85,8 @@ if Output_Plots == True:
 
     TP_Histogram(in_df, 'TP', [0, 1], ['0', '1'], [color_neg, color_pos], Input_Interactions+'.distribution_hist.png')
 
-    print('--> Saving distribution plot for output file... DONE!')
+    print('--> Saving distribution plot for output file... done!')
+
 print('Loading output file to dataframe... DONE!')
 
 # Filter output file based in similarity measure threshold
@@ -130,8 +103,12 @@ if Output_Plots == True:
 
     TP_Histogram(in_df, 'TP', [0, 1], ['0', '1'], [color_neg, color_pos], Input_Interactions+'.filtered_dist_hist.png', bins = [Opt_minSimilarity + (Opt_maxSimilarity - Opt_minSimilarity)/20 * i for i in range(0,21)])
 
-    print('--> Saving distribution plot for filtered output file... DONE!')
-print('Filtering output by {sim} in range ]{min}, {max}]... DONE!'.format(sim = Opt_SimilarityMeasure, min = str(Opt_minSimilarity), max = str(Opt_maxSimilarity)))
+    print('--> Saving distribution plot for filtered output file... done!')
+
+# Filter output file based in "TP" value
+print('Removing entries with "TP" values equal to 1...',end='\r')
+in_df = in_df[in_df['TP'] == 0]
+print('Removing entries with "TP" values equal to 1... DONE!')
 
 # Add SMILES from SMILES file to query ligand based on ChEMBL ID
 print('Adding SMILES of query ligand...', end = '\r')
@@ -144,8 +121,7 @@ in_df = pd.merge(in_df, smiles_df, on = 'Query ligand ChEMBL ID', how = 'left')
 print('Adding SMILES of query ligand... DONE!' )
 
 # Add information for query ligand and predicted target for next filters
-print('Adding useful information to entries (Pfam of known targets, max clinical phase, number of atoms, etc.)')
-print('--> Loading "broad" selection of ChEMBL into memory...', end = '\r')
+print('Adding information to entries...')
 
 Lig_info = defaultdict(dict)
 Trg_info = defaultdict(dict)
@@ -161,37 +137,76 @@ with open(Input_Broad, 'r') as ints:
         natoms 	= tokens[-2]
 
         if ligid in Lig_info:
-            Lig_info[ligid]['Known targets for ligand'].add(trgid)
-            Lig_info[ligid]['Known Pfam ID\'s for ligand'].update(pfam_id.strip().split(',')) # Convert concatenated Pfam ID's to python list
+            Lig_info[ligid]['Query ligand known targets'].add(trgid)
+            Lig_info[ligid]['Query ligand known Pfam ID\'s'].update(pfam_id.strip().split(',')) # Convert concatenated Pfam ID's to python list
 
         else:
-            Lig_info[ligid]['Known targets for ligand'] = set([trgid])
-            Lig_info[ligid]['Known Pfam ID\'s for ligand'] = set(pfam_id.strip().split(',')) # Convert concatenated Pfam ID's to python list
+            Lig_info[ligid]['Query ligand known targets'] = set([trgid])
+            Lig_info[ligid]['Query ligand known Pfam ID\'s'] = set(pfam_id.strip().split(',')) # Convert concatenated Pfam ID's to python list
             Lig_info[ligid]['Number of heavy atoms'] = natoms
-            Lig_info[ligid]['Max Clinical Phase'] = mphase
+            Lig_info[ligid]['Max Clinical Phase'] = int(mphase)
 
-        Lig_info[ligid]['Number of known targets'] = len(list(Lig_info[ligid]['Known targets for ligand']))
-        Lig_info[ligid]['Number of known Pfam ID\'s'] = len(list(Lig_info[ligid]['Known Pfam ID\'s for ligand']))
+        Lig_info[ligid]['Number of known targets'] = len(list(Lig_info[ligid]['Query ligand known targets']))
+        Lig_info[ligid]['Number of known Pfam ID\'s'] = len(list(Lig_info[ligid]['Query ligand known Pfam ID\'s']))
 
-        Trg_info[trgid]['Hit target known Pfam ID\'s'] = pfam_id
+        Trg_info[trgid]['Hit target known Pfam ID\'s'] = set(pfam_id.strip().split(','))
 
 Lig_info = pd.DataFrame.from_dict(Lig_info, orient='index')
 Lig_info['Query ligand ChEMBL ID'] = Lig_info.index
 Lig_info = Lig_info.reset_index(drop = True)
-Lig_info = Lig_info[['Query ligand ChEMBL ID', 'Number of known targets', 'Number of known Pfam ID\'s', 'Known targets for ligand', 'Known Pfam ID\'s for ligand', 'Number of heavy atoms', 'Max Clinical Phase']]
 
 Trg_info = pd.DataFrame.from_dict(Trg_info, orient='index')
 Trg_info['Hit target ChEMBL ID'] = Trg_info.index
 Trg_info = Trg_info.reset_index(drop = True)
-print('--> Loading "broad" selection of ChEMBL into memory... DONE!')
+print('--> Loading "broad" selection of ChEMBL into memory... done!')
 
 print('--> Adding "broad" information of query ligand...', end = '\r')
 in_df = pd.merge(in_df, Lig_info, on = 'Query ligand ChEMBL ID', how = 'left')
-print('--> Adding "broad" information of query ligand... DONE!')
+print('--> Adding "broad" information of query ligand... done!')
 
 print('--> Adding "broad" information of hit target...', end = '\r')
 in_df = pd.merge(in_df, Trg_info, on = 'Hit target ChEMBL ID', how = 'left')
-print('--> Adding "broad" information of hit target... DONE!')
+print('--> Adding "broad" information of hit target... done!')
+print('Adding information to entries... DONE!')
 
-print('\nComparing predicitons with ChEMBL database...', end='\r')
-df_MySQL(in_df, Input_ChEMBL)
+# Filter output file based in maximum clinical phase threshold
+print('Filtering output by Max clinical phase in range ]{min}, {max}]...'.format(min = str(Opt_minClinicalPhase), max = str(Opt_maxClinicalPhase)), end='\r')
+in_df = in_df[(in_df['Max Clinical Phase'] <= Opt_maxClinicalPhase) & (in_df['Max Clinical Phase'] > Opt_minClinicalPhase)]
+print('Filtering output by Max clinical phase in range ]{min}, {max}]... DONE!'.format(min = str(Opt_minClinicalPhase), max = str(Opt_maxClinicalPhase)))
+
+# Run MySQL query to temporally validate predicted protein-ligand interaction.
+print('Comparing predictions with ChEMBL database...', end='\r')
+db = pymysql.connect("localhost", "root", "123", Input_ChEMBL)         # Connect to MySQL server containing ChEMBL db
+
+for index, row in in_df.iterrows():                                    # Iterate over each entry of the given dataframe
+    ligid = row['Query ligand ChEMBL ID']
+    trgid = row['Hit target ChEMBL ID']
+
+    cursor = db.cursor()                                               # Create cursor for MySQL
+
+    sql_query = """SELECT * FROM activities act
+            JOIN molecule_dictionary    AS md ON act.molregno    = md.molregno
+            JOIN assays                 AS a  ON a.assay_id      = act.assay_id
+            JOIN target_dictionary      AS td ON a.tid           = td.tid
+            LEFT JOIN target_components AS tc ON td.tid          = tc.tid
+            LEFT JOIN component_domains AS cd ON tc.component_id = cd.component_id
+            LEFT JOIN domains           AS do ON cd.domain_id    = do.domain_id
+    WHERE md.chembl_id = '%s' AND td.chembl_id = '%s'""" % (ligid, trgid)
+
+    cursor.execute(sql_query)
+
+    if cursor.rowcount <= 0:
+        in_df.loc[index, 'Found in {}'.format(Input_ChEMBL)] = False
+    else:
+        in_df.loc[index, 'Found in {}'.format(Input_ChEMBL)] = True
+
+db.close()
+print('Comparing predictions with ChEMBL database... DONE!')
+
+# Compare Pfam ID's of query ligand and hit target (predicted ligand-protein pair).
+print('Comparing Pfam ID\'s of query ligand and hit target...', end='\r')
+in_df['Common Pfam ID\'s'] = in_df.apply(lambda row: [x for x in list(row['Query ligand known Pfam ID\'s']) if x in list(row['Hit target known Pfam ID\'s'])], axis=1) # List common Pfam ID's between query ligand and hit target (predicted ligand-protein pair).
+in_df['Number of common Pfam ID\'s'] = in_df.apply(lambda row: len(row['Common Pfam ID\'s']), axis=1) # Count common Pfam ID's for further filtering down the line.
+print('Comparing Pfam ID\'s of query ligand and hit target... DONE!')
+
+# 
